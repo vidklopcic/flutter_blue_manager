e4808f042cfe80197d1750b83c127903818bb25e
vidklopcic <klopcic.vid@gmail.com>
2020-01-23 09:39:21 +0100

    call initFromScanresult on every autoconnect

--- lib/flutter_blue_manager.dart
+++ lib/flutter_blue_manager.dart
@@ -181,7 +181,7 @@ class FlutterBlueManager {
     debug('connecting ${scanResult.device?.name}', FBMDebugLevel.info);
 
     FBMLock lock = await getBleLock();
-    if (device.scanResult == null) device.initFromScanResult(scanResult);
+    device.initFromScanResult(scanResult);
     if (scanResult.device == null) {
       _autoConnectHandled.remove(uuid);
       lock.unlock();
